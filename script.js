3828cd2b06dfc8d653b4d997036c356eed7320ce
Stephenna J Corry <Sjcstephenna@gmail.com>
2021-02-17 23:27:39 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,23 @@
+const navSlide = () =>{
+    // Selectors
+    const burger = document.querySelector(".burger");
+    const nav = document.querySelector(".nav-links");
+    const navLinks = document.querySelectorAll(".nav-links li");
+
+    // Toggle Nav
+    burger.addEventListener('click', () => {
+        nav.classList.toggle('nav-active');
+
+    // Animate Links
+       navLinks.forEach((nLink, i) => {
+           if(nLink.style.animation){
+               nLink.style.animation = "";
+           } else {
+               nLink.style.animation = `navLinkFade 0.5s ease forwards ${i / 7 + 0.5}s`;
+           }
+       });
+       burger.classList.toggle('toggle')
+    });
+}
+
+navSlide();
